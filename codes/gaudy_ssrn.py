e32a1d9c06d13488b7f65d98837d4673b4923de2
Łukasz Nawaro <l.nawaro@delab.uw.edu.pl>
2019-03-25 10:17:58 +0000

    SSRN

---
+++ codes/gaudy_ssrn.py
@@ -0,0 +1,119 @@
+from gaudy_functions import *
+import os
+from time import sleep
+
+# Parameters to use later.
+#
+# Assign weights so that no site skews the results too much
+# In our case, we also use it to give higher priority to European sources
+# Save weights to pickle, because we use it later
+# Timeframe beginning (inclusive) and end (exclusive)
+# Directories to save results in (create if do not exist)
+
+weights = {
+    'ssrn': 1.0,
+}
+
+analysis_begin = '2016-01-01'  # inclusive
+analysis_end = '2019-03-01'  # exclusive
+
+res = './res_ssrn_0309/'
+assets = './assets_ssrn_0309/'
+grams = {1: './mod_ssrn_0309/', 2: './mod-bi_ssrn_0309/'}
+make_directory(res)
+make_directory(assets)
+for gram_dir_name in grams.values():
+    make_directory(gram_dir_name)
+if abs(sum(weights.values()) - 1) > 0.000001:
+    raise Exception('Sum of weights is not 1', sum(weights.values()))
+
+important_1 = ['leaveeu','aidriven', 'gdpr','farright', 'metoo', 'misinform', 'decentr', 'monopoli',   ]
+important_1 = list(set(important_1))
+
+important_2 = ['ai startup', 'chines tech', 'digit health', 'autonom weapon', 'internet freedom', 'network neutral',
+               'fake news', 'conspiraci theori', 'hate speech', 'person data', 'privaci polici', 'russian interfer',
+               'youtub kid', 'black box', 'cambridg analytica', 'black box', 'money launder', 'distribut ledger' ]
+important_2 = list(set(important_2))
+
+sentiment_words = important_1
+
+sentiment_bigrams = important_2
+
+
+def prepare():
+    df_april = pd.read_pickle('../engineroom_science/src/ssrn_april.p')
+    df_april['date'] = df_april['date'].apply(lambda x: pd.to_datetime(x, format='%d-%m-%Y')).astype(str)
+    print(df_april['date'])
+
+    dfs = [
+        df_april,
+        pd.read_csv('../engineroom_science/src/ssrn_text_mp.csv'),
+        pd.read_csv('../engineroom_science/src/ssrn_textkg.csv'),
+        pd.read_csv('../engineroom_science/new_ssrn_text1.csv'),
+        pd.read_csv('../engineroom_science/new_ssrn_text2.csv'),
+        pd.read_csv('../scraping_science/20171209_scraping_ssrn/ssrn_text_190309.csv'),
+    ]
+    print([x.shape for x in dfs])
+    df = pd.concat(dfs, axis=0)
+    df['text'] = df['abstract']
+    df['date'] = df['date'].apply(lambda x: x.replace('Posted: ', ''))
+    df = df.loc[df['date'] != 'Posted:']
+    print(df.shape)
+    # for date in df['date']:
+    #     try:
+    #         pd.to_datetime(date)
+    #     except Exception as ex:
+    #         print(ex, date)
+    df = initial_transform(df, analysis_begin, analysis_end, date_columns=['date', ], title_columns=['title', ])
+    df = months_columns(df)
+    df['link'] = 'ssrn'
+    df['site'] = 'ssrn'
+    df['author'] = ''
+    save_months(df, assets_dir=assets)
+    save_frequencies(df, gram_dirs=grams, weights=weights, assets_dir=assets, res_dir=res)
+
+
+def concat():
+    save_concat(gram_dirs=grams, assets_dir=assets, res_dir=res, weights=weights)
+
+
+def regression():
+    reg(gram_dirs=grams, assets_dir=assets, res_dir=res)
+
+
+def co_occurrence():
+    for ngram, words in ((2, important_2), (1, important_1)):
+        for ngram_compare, _ in ((1, important_1), (2, important_2)):
+            cooc(ngram, ngram_compare, words, assets, grams, res, weights)
+
+
+def sentiments():
+    sentiment(important_1, weights=weights, gram_dirs=grams, assets_dir=assets, res_dir=res, ngram=1,
+              ngram_compare=2)
+    sentiment(important_2, weights=weights, gram_dirs=grams, assets_dir=assets, res_dir=res, ngram=2,
+              ngram_compare=2)
+    sentiment(important_1, weights=weights, gram_dirs=grams, assets_dir=assets, res_dir=res)
+    sentiment(important_2, weights=weights, gram_dirs=grams, assets_dir=assets, res_dir=res, ngram=2,
+              ngram_compare=1)
+    
+prepare()
+concat()
+regression()
+co_occurrence()
+sentiments()
+
+# print('new_ssrn_text1.csv' in os.listdir('../engineroom_science/'))
+
+# while True:
+#     if 'new_ssrn_text2.csv' in os.listdir('../engineroom_science/'):
+#         sleep(30)
+#         prepare()
+#         concat()
+#         regression()
+#         co_occurrence()
+#         sentiments()
+#         break
+#     else:
+#         print('new2', 'new_ssrn_text2.csv' in os.listdir('../engineroom_science/'),
+#               'new1', 'new_ssrn_text1.csv' in os.listdir('../engineroom_science/'))
+#         sleep(60)
